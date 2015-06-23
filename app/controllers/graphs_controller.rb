dcfe4fd22da115bb496725cc94dff2bac1af46f8
jackfjo3 <jackfjo3@gmail.com>
2015-06-23 10:55:12 -0600

    min and max chart y values now dynamicaly set. need to fix chart display when no points exist yet

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -9,6 +9,13 @@ class GraphsController < ApplicationController
   def show
     @graph = Graph.find(params[:id])
     @trackables = @graph.trackables.all
+    @trackables.each do |trackable|
+      points = trackable.points.all
+      points.each do |point|
+        @y_max == nil || point.value > @y_max ? @y_max = point.value : @y_max = @y_max #seems redundant to assign to itself
+        @y_min == nil || point.value < @y_min ? @y_min = point.value : @y_min = @y_min
+      end
+    end
   end
 
   def new
